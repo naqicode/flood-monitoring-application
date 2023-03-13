1d8c6a665d11bc2719164b2cba58fb19a9151d69
naqicode <naqicode19972020@gmail.com>
2023-03-13 12:32:49 +0000

    Displaying the Flood Warning alerts on the third page

--- src/pages/PageThree.js
+++ src/pages/PageThree.js
@@ -4,6 +4,42 @@ function PageThree() {
   return (
     <div>
       <h1>HJIDKODKD</h1>
+
+      SO BASICALLY - TRY TO USE MAP THE POLYGON OF THIS API "https://environment.data.gov.uk/flood-monitoring/id/floods".
+      WHICH THEN SHOULD GIVE OUT LAT AND LONG OF THOSE AREAS WITH WARNING.
+
+
+
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        margin: '10px',
+      }}
+    >
+          <div
+            style={{
+              height: '75vh',
+              backgroundColor: 'lightBlue',
+              width: '80vw',
+            }}
+          >
+              
+          </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   )
 }
