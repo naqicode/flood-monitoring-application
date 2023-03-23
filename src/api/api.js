697b40e7e24db7ceaced308425c7eb10ae88c3a7
naqicode <naqicode19972020@gmail.com>
2023-03-23 23:35:23 +0000

    Added comments to the 'api.js' file

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 // File for rendering APIs in redux
 
-//An 'Async await' function to only return resonse.json() if response is ok or else return the status
+//An 'Async Await' function to only return resonse.json() if response is ok or else return the status
 async function checkAndReturn(response) { //async function with 'response' as param.
   if (response.ok) { //if statement to check if the 'response' param is ok
     //The result of response.json() is set to a new var 'jsonResp' - 
@@ -13,28 +13,34 @@ async function checkAndReturn(response) { //async function with 'response' as pa
 }
 
 
-export const counties = {
+
+//Exporting counties so it can be used in other files
+//And sets it to 2 Async function
+export const counties = { 
+  //An 'Async Await' function 
   async getFloodmonitoring() {
-    const resp = await fetch(
+    const resp = await fetch( //Uses 'await' to fetch the API and set it to a new variable 'resp'.
       `https://environment.data.gov.uk/flood-monitoring/id/floodAreas`
     );
-    const data = checkAndReturn(resp);
-    return data;
+    //uses the checkAndReturn() function to run resp as a param 
+    //and set it to new var 'data'
+    const data = checkAndReturn(resp); 
+    return data; //returns data
   },
 
+  //An 'Async Await' function 
   async getFloodWarnings () {
+    //Uses 'await' to fetch the API and set it to a new variable 'resp'.
     const resp = await fetch(`https://environment.data.gov.uk/flood-monitoring/id/floods`);
+      //uses the checkAndReturn() function to run resp as a param and set it to new var 'data'
     const data = checkAndReturn(resp);
-    return data
+    return data //returns data
   }
 };
 
 
-//
-
+//sets the counties var to a new var 'api'
 const api = { counties };
 
-export default api;
+export default api; //exporting the api
 
-
-//https://environment.data.gov.uk/flood-monitoring/id/floods
