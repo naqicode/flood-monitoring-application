7169332cb589aa47ea656315acd99a3d2d18fd30
naqicode <naqicode19972020@gmail.com>
2023-03-27 16:42:48 +0100

    Added comments

--- src/redux/countySlice.js
+++ src/redux/countySlice.js
@@ -1,23 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit"; //imports create slice
 
+//Create slice is created and set to a new var 'countySlice'
+//The createSlice function accepts a slice name, an initial state, and a action reducer -
+// - with action creators to respond to action state 
+//it automatically generates action creators and action types - 
+// - that responds to the reducers and state
+// The const 'countySlice' is assinged the value returned by 'createSlice' function
 const countySlice = createSlice({
-  name: "county list",
-  initialState: {
+  name: "county list", //The name of the slice
+  initialState: { //intial state of the slice, which is an object with single property 'countyList'  set to empty array
     countyList: [],
 
   },
-  reducers: {
+  reducers: { //action reducers - An object that defines the action that can be performed on the state of the slice
+    //action creator called 'addCounties' which is a function
+    //The type property 'state' - describes the action
+    //The payload property contains the data necessary to update the state
     addCounties(state, action) {
-      state.countyList = action.payload;
+      state.countyList = action.payload; //The payload is an array of counties and the action updates the countyList
     },
 
   },
 });
 
-
+// Takes entire 'state' as an input - and returns the county from countyListReducer
 export const selectorCountyListNew = (state) => state.countyListReducer.county
 
-
+// Exports action creator function 'addCounties' from the 'countySlice' slice.
 export const {addCounties} = countySlice.actions
 
+// Exports the reducer function from the 'countySlice' slice. which is responsible -
+// - for handling actions and updating the 'state'.
 export default countySlice.reducer
--- src/redux/floodAreaSlice.js
+++ src/redux/floodAreaSlice.js
@@ -1,14 +1,23 @@
+// Import createSlice function from redux toolkit
 import { createSlice } from "@reduxjs/toolkit";
 
+// The floodAreaSlice is assigned the return value of createSlice() function
+// THe createSlice() function consists of slice name, the intiial state and action reducers.
 const floodAreaSlice = createSlice({
-  name: "floodArea",
+  name: "floodArea", // The name of the slice
+  // Initial state of the slice which is an object with 2 properties.
+  // The properties are set to empty array
   initialState: {
     county: [],
     floodWarning: [],
   },
+  // Action reducers - defines actions that can be performed on the state of slice
   reducers: {
+    // 'addCounty' action creator
+    // 'addFloodWarning' action crrator
+    // the 'state' param describes the state and action describes the action
     addCounty(state, action) {
-      state.county = action.payload;
+      state.county = action.payload; // The result of action.payload is set to state.county
     },
     addFloodWarning (state, action) 
     {
@@ -17,10 +26,15 @@ const floodAreaSlice = createSlice({
   },
 });
 
-
+// Takes entire 'state' as an input - and returns the county from floodAreaReducer
 export const selectorCountyList = (state) => state.floodAreaReducer.county
+
+// Takes entire 'state' as an input - and returns the floodWarning from floodAreaReducer
 export const selectorFloodWarning = (state) => state.floodAreaReducer.floodWarning
 
+// Exports action creator function 'addCounties' and 'addFloodWarning' from the 'floodAreaSlice' slice.
 export const {addCounty, addFloodWarning} = floodAreaSlice.actions
 
+// Exports the reducer function from the 'floodAreaSlice' slice. which is responsible -
+// - for handling actions and updating the 'state'.
 export default floodAreaSlice.reducer
