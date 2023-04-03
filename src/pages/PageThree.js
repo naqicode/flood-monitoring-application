c6b7d9b6ed0d024225f2e966152b919f93627751
naqicode <naqicode19972020@gmail.com>
2023-04-03 13:14:24 +0100

    Added css to the third page

--- src/pages/PageThree.js
+++ src/pages/PageThree.js
@@ -49,12 +49,6 @@ const handleClick = (warning) => {
 };
 
 
-//AADED
-// const handleClick = () => {
-//   warningData.map((e) => {
-//     console.log(e.floodArea.riverOrSea)
-//   })
-// }
 
 
 
@@ -66,115 +60,399 @@ const handleClick = (warning) => {
       WHICH THEN SHOULD GIVE OUT LAT AND LONG OF THOSE AREAS WITH WARNING.
 
 
-
-
-      <div className="warning-list-container"
-        style={{
-          overflowY: 'scroll',
-          maxHeight: '745px',
-          border: '1px solid red',
-          paddingBottom: '10px',
-          width: '25vw'
-        }}
+    <div
+      style={{
+        display: 'flex'
+      }}
       >
-
-          {/* ADDED NEW */}
-
-          <ul className="warning-list" style={{ listStyleType: 'none', padding: 0, margin: 0, display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-            <h3>Alerts</h3>
-
-            {warningData &&
+      <ul
+        style={{
+          listStyle: 'none',
+          overflowY: 'scroll',
+          maxHeight: '75vh',
+          maxWidth: '25vw',
+          paddingRight: '20px'
+        }}
+        >
+        <h3
+          style={{
+            fontSize: '2em',
+            textAlign: 'center',
+            marginBottom: '1em'
+          }}
+        >ALERTS</h3>
+        {warningData &&
             warningData
             .sort((a, b) => new Date(b.timeRaised) - new Date(a.timeRaised))
             .map((ele, index) => (
-              <li style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}} key={index}>
-                <button 
-                  onClick={() => handleClick(ele)}
-                  
-                  style={{ 
-                    borderRadius: '30px',
-                    fontSize: '20px',
-                    padding: '20px', 
-                    width: '200px', 
-                    margin: '10px' 
-                    }}>
-                    {ele.timeRaised}
+              <li key={index}>
+                <button onClick={() => handleClick(ele)}
+                  style={{
+                    width: '100%',
+                    height: '150px',
+                    lineHeight: '75px',
+                    border: 'none',
+                    borderRadius: '10px',
+                    backgroundColor: '#f44336',
+                    color: '#fff',
+                    textTransform: 'uppercase',
+                    fontWeight: 'bold',
+                    letterSpacing: '1px',
+                    boxShadow: '0px 4px 5px rgba(0, 0, 0, 0.1)',
+                    marginBottom: '10px',
+                    textAlign: 'center',
+                    fontSize: '1.2em',
+                    transition: 'all 0.2s ease-in-out'
+                  }}
+                  onMouseEnter={(e) => {
+                    e.target.style.backgroundColor = '#b71c1c';
+                    e.target.style.cursor = 'pointer';
+                  }}
+                  onMouseLeave={(e) => {
+                    e.target.style.backgroundColor = '#f44336';
+                  }}
+                  >
+                    <span style={{padding: '5px'}}>{ele.timeRaised}</span>
                 </button>
               </li>
             ))}
+              
+      </ul>
 
-          </ul>
-        
-      </div>
+   
 
-      <div className="warning-details-container"
-        style={{
-          border: '1px solid red',
-          display: 'flex',
-          justifyContent: 'flex-end',
-          alignItems: 'inherit',
-          width: '50vw',
-          position: 'fixed',
-          top: '200px',
-          right: '200px'
 
-          
-         }}
-        >
+
+    <div 
+      style={{
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+        }}
+      >
       {selectedWarning && (
-          <div className="warning-details"
-            style={{
-              display: 'flex',
-              justifyContent: 'flex-start',
-              alignItems: 'flex-start',
-              flexDirection: 'column'
+          <div 
+          style={{
+            backgroundColor: '#ffebee',
+            border: '1px solid #e53935',
+            borderRadius: '10px',
+            padding: '20px',
+            margin: '20px',
+            width: '80%',
+            maxWidth: '800px',
+            boxShadow: '0 0 20px rgba(0, 0, 0, 0.2)',
+            overflowY: 'auto',
+            maxHeight: '60vh'
+
+          }}
+          >
+            <h3 style={{ 
+              color: '#e53935',
+              textAlign: 'center',
+              fontSize: '24px',
+              fontWeight: 'bold',
+              marginBottom: '20px',
+              textTransform: 'uppercase'
+             }}>Warning Details</h3>
+
+
+
+
+             {/* SEPERATE */}
+             <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                marginBottom: '10px',
+                backgroundColor: 'rgba(242, 36, 36, 0.3)',
+                padding: '10px',
+                borderRadius: '10px'
+              }}>
+                <p style={{
+                  fontWeight: 'bold',
+                  marginRight: '10px',
+                  marginBottom: '5px',
+                  textTransform: 'uppercase'
+                }}>ID:</p>
+                  <p>{selectedWarning.id}</p>
+              </div>
+
+              {/* SEPERATE <DIV/> */}
+              <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                marginBottom: '10px',
+                backgroundColor: 'rgba(242, 36, 36, 0.3)',
+                padding: '10px',
+                borderRadius: '10px'
+              }}>
+                <p style={{
+                  fontWeight: 'bold',
+                  marginRight: '10px',
+                  marginBottom: '5px',
+                  textTransform: 'uppercase'
+                }}>Description:</p>
+                  <p>{selectedWarning.description}</p>
+              </div>
               
 
-            }}
+              {/* SEPERATE <DIV/> */}
+              <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                marginBottom: '10px',
+                backgroundColor: 'rgba(242, 36, 36, 0.3)',
+                padding: '10px',
+                borderRadius: '10px'
+              }}>
+                <p style={{
+                  fontWeight: 'bold',
+                  marginRight: '10px',
+                  marginBottom: '5px',
+                  textTransform: 'uppercase'
+                }}>EA AreaName:</p>
+                  <p>{selectedWarning.eaAreaName}</p>
+              </div>
 
-          >
-            <h3>Warning Details</h3>
-            <p>ID: {selectedWarning.id}</p>
-            <p>Description: {selectedWarning.description}</p>
-            <p>EA Area Name: {selectedWarning.eaAreaName}</p>
-            <p>EA Region Name: {selectedWarning.eaRegionName}</p>
-            <p>Flood Area County: {selectedWarning.floodArea.county}</p>
-            <p>Flood Notation: {selectedWarning.floodArea.notation}</p>
-            <p>River or Sea: {selectedWarning.floodArea.riverOrSea}</p>
-            <p>Flood Area ID: {selectedWarning.floodAreaID}</p>
-            <p>Is it Tidal: {selectedWarning.isTidal}</p>
-            <p>Message: {selectedWarning.message}</p>
-            <p>Severity: {selectedWarning.severity}</p>
-            <p>Severity Level: {selectedWarning.severityLevel}</p>
-            <p>Time Message Changed: {selectedWarning.timeMessageChanged}</p>
-            <p>Time Message Raised: {selectedWarning.timeRaised}</p>
-            <p>Time SeverityChanged: {selectedWarning.timeSeverityChanged}</p>
+               {/* SEPERATE <DIV/> */}
+               <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                marginBottom: '10px',
+                backgroundColor: 'rgba(242, 36, 36, 0.3)',
+                padding: '10px',
+                borderRadius: '10px'
+              }}>
+                <p style={{
+                  fontWeight: 'bold',
+                  marginRight: '10px',
+                  marginBottom: '5px',
+                  textTransform: 'uppercase'
+                }}>EA RegionName:</p>
+                  <p>{selectedWarning.eaRegionName}</p>
+              </div>
+
+              {/* SEPERATE <DIV/> */}
+              <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                marginBottom: '10px',
+                backgroundColor: 'rgba(242, 36, 36, 0.3)',
+                padding: '10px',
+                borderRadius: '10px'
+              }}>
+                <p style={{
+                  fontWeight: 'bold',
+                  marginRight: '10px',
+                  marginBottom: '5px',
+                  textTransform: 'uppercase'
+                }}>County:</p>
+                  <p>{selectedWarning.floodArea.county}</p>
+              </div>
+              
+              {/* SEPERATE <DIV/> */}
+              <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                marginBottom: '10px',
+                backgroundColor: 'rgba(242, 36, 36, 0.3)',
+                padding: '10px',
+                borderRadius: '10px'
+              }}>
+                <p style={{
+                  fontWeight: 'bold',
+                  marginRight: '10px',
+                  marginBottom: '5px',
+                  textTransform: 'uppercase'
+                }}>Notation:</p>
+                  <p>{selectedWarning.floodArea.notation}</p>
+              </div>
+
+              {/* SEPERATE <DIV/> */}
+              <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                marginBottom: '10px',
+                backgroundColor: 'rgba(242, 36, 36, 0.3)',
+                padding: '10px',
+                borderRadius: '10px'
+              }}>
+                <p style={{
+                  fontWeight: 'bold',
+                  marginRight: '10px',
+                  marginBottom: '5px',
+                  textTransform: 'uppercase'
+                }}>River Or Sea:</p>
+                  <p>{selectedWarning.floodArea.riverOrSea}</p>
+              </div>
+
+                            {/* SEPERATE <DIV/> */}
+                            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                marginBottom: '10px',
+                backgroundColor: 'rgba(242, 36, 36, 0.3)',
+                padding: '10px',
+                borderRadius: '10px'
+              }}>
+                <p style={{
+                  fontWeight: 'bold',
+                  marginRight: '10px',
+                  marginBottom: '5px',
+                  textTransform: 'uppercase'
+                }}>Flood Area ID:</p>
+                  <p>{selectedWarning.flodAreaID}</p>
+              </div>
+
+              {/* SEPERATE <DIV/> */}
+              <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                marginBottom: '10px',
+                backgroundColor: 'rgba(242, 36, 36, 0.3)',
+                padding: '10px',
+                borderRadius: '10px'
+              }}>
+                <p style={{
+                  fontWeight: 'bold',
+                  marginRight: '10px',
+                  marginBottom: '5px',
+                  textTransform: 'uppercase'
+                }}>Is it Tidal:</p>
+                  <p>{selectedWarning.isTidal}</p>
+              </div>
+
+              {/* SEPERATE <DIV/> */}
+              <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                marginBottom: '10px',
+                backgroundColor: 'rgba(242, 36, 36, 0.3)',
+                padding: '10px',
+                borderRadius: '10px'
+              }}>
+                <p style={{
+                  fontWeight: 'bold',
+                  marginRight: '10px',
+                  marginBottom: '5px',
+                  textTransform: 'uppercase'
+                }}>Message:</p>
+                  <p>{selectedWarning.message}</p>
+              </div>
+
+              {/* SEPERATE <DIV/> */}
+              <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                marginBottom: '10px',
+                backgroundColor: 'rgba(242, 36, 36, 0.3)',
+                padding: '10px',
+                borderRadius: '10px'
+              }}>
+                <p style={{
+                  fontWeight: 'bold',
+                  marginRight: '10px',
+                  marginBottom: '5px',
+                  textTransform: 'uppercase'
+                }}>Severity:</p>
+                  <p>{selectedWarning.severity}</p>
+              </div>
+
+              {/* SEPERATE <DIV/> */}
+              <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                marginBottom: '10px',
+                backgroundColor: 'rgba(242, 36, 36, 0.3)',
+                padding: '10px',
+                borderRadius: '10px'
+              }}>
+                <p style={{
+                  fontWeight: 'bold',
+                  marginRight: '10px',
+                  marginBottom: '5px',
+                  textTransform: 'uppercase'
+                }}>severityLevel:</p>
+                  <p>{selectedWarning.severityLevel}</p>
+              </div>
+
+              {/* SEPERATE <DIV/> */}
+              <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                marginBottom: '10px',
+                backgroundColor: 'rgba(242, 36, 36, 0.3)',
+                padding: '10px',
+                borderRadius: '10px'
+              }}>
+                <p style={{
+                  fontWeight: 'bold',
+                  marginRight: '10px',
+                  marginBottom: '5px',
+                  textTransform: 'uppercase'
+                }}>Time Messaged Changed:</p>
+                  <p>{selectedWarning.timeMessageChanged}</p>
+              </div>
+
+              {/* SEPERATE <DIV/> */}
+              <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                marginBottom: '10px',
+                backgroundColor: 'rgba(242, 36, 36, 0.3)',
+                padding: '10px',
+                borderRadius: '10px'
+              }}>
+                <p style={{
+                  fontWeight: 'bold',
+                  marginRight: '10px',
+                  marginBottom: '5px',
+                  textTransform: 'uppercase'
+                }}>Time Message Raised:</p>
+                  <p>{selectedWarning.timeRaised}</p>
+              </div>
+
+              {/* SEPERATE <DIV/> */}
+              <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                marginBottom: '10px',
+                backgroundColor: 'rgba(242, 36, 36, 0.3)',
+                padding: '10px',
+                borderRadius: '10px'
+              }}>
+                <p style={{
+                  fontWeight: 'bold',
+                  marginRight: '10px',
+                  marginBottom: '5px',
+                  textTransform: 'uppercase'
+                }}>Time Severity Level Changed:</p>
+                  <p>{selectedWarning.timeSeverityChanged}</p>
+              </div>
+
 
             </div>
       )}
+
       </div>
 
-
-
-
-
-  
-
-
-
-
-
-            
-
-
-
-
-
-
-
-
-
-
+      </div>
     </div>
   )
 }
